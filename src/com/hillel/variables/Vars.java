26a5ccea5763194a68a75b010c2530496b616a6f
Oleg Mikhailov <Oleg Mikhailov>
2016-12-08 20:53:58 +0200

    first save

---
+++ src/com/hillel/variables/Vars.java
@@ -0,0 +1,19 @@
+  package com.hillel.variables;
+
+    public class Vars {
+      public static void main(String[] args){
+      /*  byte var = 127;
+      int num = var + 4;
+
+        byte anotherVar = (byte)num;
+        System.out.println(anotherVar);
+        */
+        float var = 3.5f;
+        double d = 14.56;
+        System.out.println(var);
+        int a = 30;
+        int b = 25;
+        int c = a/b;
+          System.out.println(c);
+    }
+}
