ea15464c85febbb71b3e6c8044670c9617a135a6
Mikhailov <mikhailov.oleg.m@gmail.com>
2016-12-09 20:00:38 +0200

    first homework

---
+++ src/com/hillel/homework/FirstEx.java
@@ -0,0 +1,11 @@
+package com.hillel.homework;
+
+  public class FirstEx{
+    public static void main(String[] args){
+      int V=120;
+      int T=2;
+      int S=0;
+      S = V*T;
+        System.out.println("Speed: " +S);
+  }
+}
