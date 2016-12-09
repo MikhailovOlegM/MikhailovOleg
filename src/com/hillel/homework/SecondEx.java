05e3aa651c95894030871474972a1b815b74c8e7
Mikhailov <mikhailov.oleg.m@gmail.com>
2016-12-09 20:49:00 +0200

    second

---
+++ src/com/hillel/homework/SecondEx.java
@@ -0,0 +1,18 @@
+package com.hillel.homework;
+import java.util.Scanner;
+
+  public class SecondEx{
+    public static void main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input 1 cathet: ");
+        int a = in.nextInt();
+        System.out.print("Input 2 cathet: ");
+        int b = in.nextInt();
+        System.out.print("Input hypotenuse: ");
+        int c = in.nextInt();
+        int S = (a+b)/2;
+        int P = a+b+c;
+        System.out.println("S=" + S + "  P=" +P);
+    }
+}
